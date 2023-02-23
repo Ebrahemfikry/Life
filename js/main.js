fb422d9d126e911944aab719a27c7c34a8b9de7f
Ebrahem Fikry <86515941+Ebrahemfikry@users.noreply.github.com>
2023-02-23 13:23:31 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let menu1 = document.getElementById("menu1");
+let menu2 = document.getElementById("menu2");
+let menu3 = document.getElementById("menu3");
+let menu4 = document.getElementById("menu4");
+let menu5 = document.getElementById("menu5");
+
+function showOne() {
+    menu1.classList.toggle("mystyle");
+}
+function showTwo() {
+    menu2.classList.toggle("mystyle");
+}
+function showThree() {
+    menu3.classList.toggle("mystyle");
+}
+function showFour() {
+    menu4.classList.toggle("mystyle");
+}
+function showFive() {
+    menu5.classList.toggle("mystyle");
+}
